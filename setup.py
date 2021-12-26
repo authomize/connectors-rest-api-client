2158c3347fb7a7d94f1858db55baa6707d9850ec
Vladimir Zoubritsky <vladimir.zoubritsky@authomize.com>
2021-12-26 16:25:42 +0200

    Update template

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import sys
 
 from setuptools import find_namespace_packages, setup
 
-if not sys.version_info[:2] == (3, 8):
-    sys.exit('Sorry, Python != 3.8 is not supported')
-
 if __name__ == '__main__':
     setup(
         version='0.0.1',
@@ -14,13 +11,13 @@ if __name__ == '__main__':
         description='Fill in the relevent description',
         packages=find_namespace_packages(include=['authomize.*']),
         package_data={'authomize.fill_the_right_name': ['config/*.yaml']},
-        python_requires='==3.8.*',
+        python_requires='>=3.8',
         install_requires=[],
         extras_require={
             'test': [
                 'coverage~=5.2',
-                'flake8~=3.8',
-                'flake8-isort~=3.0',
+                'flake8~=4.0',
+                'flake8-isort~=4.0',
                 'pyhamcrest~=2.0',
                 'pytest~=5.4',
                 'pytest-html~=2.1',
