974c08a20ec67691c0c9f3d415c101169a196f9a
vogre <vladimir.zoubritsky@authomize.com>
2022-05-25 21:26:33 +0300

    Publish package to pypi on merge (#8)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 if __name__ == '__main__':
     setup(
-        version='0.0.8',
+        version='0.1.0',
         name='authomize-rest-api-client',
         author='Authomize inc.',
         license='MIT',
