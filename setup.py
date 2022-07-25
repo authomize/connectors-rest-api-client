3022a2cfe04449b2a635b9fccbfc0643ec666594
RoiAuthomize <90147027+RoiAuthomize@users.noreply.github.com>
2022-07-25 16:20:52 +0300

    Add department field to the Identity model AUTH-4967 (#11)
    * Add department field to the Identity model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 if __name__ == '__main__':
     setup(
-        version='1.0.2',
+        version='1.1.0',
         name='authomize-rest-api-client',
         author='Authomize inc.',
         license='MIT',
