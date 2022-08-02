a8aeada02d96212d90ee7337693fc423ae90aab7
RoiAuthomize <90147027+RoiAuthomize@users.noreply.github.com>
2022-08-03 00:09:02 +0300

    Add new endpoints for access, association and assets (#12)

--- authomize/rest_api_client/client/client.py
+++ authomize/rest_api_client/client/client.py
@@ -5,12 +5,16 @@ from apiclient_pydantic import serialize_all_methods, serialize_response
 from authomize.rest_api_client.client.base_client import BaseClient
 from authomize.rest_api_client.generated.schemas import (
     BundleTransactionSchema,
+    IsAliveResponse,
     ItemsBundleSchema,
     MeResponse,
-    NewGroupMembersRequestSchema,
-    NewGroupMembersResponseSchema,
-    NewGroupRequestSchema,
-    NewGroupResponseSchema,
+    NewAccountsAssociationRequestSchema,
+    NewAssetsInheritanceRequestSchema,
+    NewAssetsRequestSchema,
+    NewGroupingRequestSchema,
+    NewGroupingResponseSchema,
+    NewGroupsAssociationRequestSchema,
+    NewPermissionsRequestSchema,
     NewUserRequestSchema,
     NewUserResponseSchema,
     RestApiConnectorListSchema,
@@ -20,21 +24,30 @@ from authomize.rest_api_client.generated.schemas import (
 
 @serialize_all_methods(decorator=serialize_response)
 class Client(BaseClient):
-    def me(self) -> MeResponse:
-        return self.http_get('/v1/me')
+    def is_alive(self) -> IsAliveResponse:
+        return self.http_get('/is_alive')
 
-    def list_connectors(self, params=None) -> RestApiConnectorListSchema:
+    def me(self) -> MeResponse:
+        return self.http_get('/me')
+
+    def list_connectors(
+        self,
+        params=None,
+    ) -> RestApiConnectorListSchema:
         return self.http_get('/v1/connectors', params=params)
 
-    def create_transaction(self, connector_id: str) -> BundleTransactionSchema:
+    def create_transaction(
+        self,
+        connector_id: str,
+    ) -> BundleTransactionSchema:
         if not connector_id:
             raise ValueError('Missing connector_id')
         return self.http_post(f'/v1/connectors/{connector_id}/transactions')
 
     def retrieve_transaction(
-            self,
-            connector_id: str,
-            transaction_id: str
+        self,
+        connector_id: str,
+        transaction_id: str,
     ) -> BundleTransactionSchema:
         if not connector_id:
             raise ValueError('Missing connector_id')
@@ -42,7 +55,11 @@ class Client(BaseClient):
             raise ValueError('Missing transaction_id')
         return self.http_get(f'/v1/connectors/{connector_id}/transactions/{transaction_id}')
 
-    def apply_transaction(self, connector_id: str, transaction_id: str) -> BundleTransactionSchema:
+    def apply_transaction(
+        self,
+        connector_id: str,
+        transaction_id: str,
+    ) -> BundleTransactionSchema:
         if not connector_id:
             raise ValueError('Missing connector_id')
         if not transaction_id:
@@ -50,10 +67,10 @@ class Client(BaseClient):
         return self.http_post(f'/v1/connectors/{connector_id}/transactions/{transaction_id}/apply')
 
     def extend_transaction_items(
-            self,
-            connector_id: str,
-            transaction_id: str,
-            items: ItemsBundleSchema
+        self,
+        connector_id: str,
+        transaction_id: str,
+        items: ItemsBundleSchema,
     ) -> SubmitResponse:
         if not connector_id:
             raise ValueError('Missing connector_id')
@@ -61,29 +78,76 @@ class Client(BaseClient):
             raise ValueError('Missing transaction_id')
         return self.http_post(
             f'/v1/connectors/{connector_id}/transactions/{transaction_id}/items',
-            body=items.json()
+            body=items.json(),
         )
 
-    def delete_app_data(self, app_id: str):
+    def delete_app_data(
+        self,
+        app_id: str,
+    ):
         if not app_id:
             raise ValueError('Missing app_id')
         return self.http_delete(url=f'/v2/apps/{app_id}/data')
 
-    def create_users(self, app_id: str, body: list[NewUserRequestSchema]) -> NewUserResponseSchema:
+    def create_users(
+        self,
+        app_id: str,
+        body: list[NewUserRequestSchema],
+    ) -> NewUserResponseSchema:
         if not app_id:
             raise ValueError('Missing app_id')
         return self.http_post(url=f'/v2/apps/{app_id}/accounts/users', body=json.dumps(body))
 
-    def create_groups(
-            self, app_id: str, body: list[NewGroupRequestSchema]
-    ) -> NewGroupResponseSchema:
+    def create_groupings(
+        self,
+        app_id: str,
+        body: list[NewGroupingRequestSchema],
+    ) -> NewGroupingResponseSchema:
         if not app_id:
             raise ValueError('Missing app_id')
-        return self.http_post(url=f'/v2/apps/{app_id}/groups', body=json.dumps(body))
+        return self.http_post(url=f'/v2/apps/{app_id}/access/grouping', body=json.dumps(body))
 
-    def create_groups_members(
-            self, app_id: str, body: NewGroupMembersRequestSchema
-    ) -> NewGroupMembersResponseSchema:
+    def create_permissions(
+        self,
+        app_id: str,
+        body: list[NewPermissionsRequestSchema],
+    ):
         if not app_id:
             raise ValueError('Missing app_id')
-        return self.http_post(url=f'/v2/apps/{app_id}/groups/members', body=body.json())
+        return self.http_post(url=f'/v2/apps/{app_id}/access/permissions', body=json.dumps(body))
+
+    def create_accounts_association(
+        self,
+        app_id: str,
+        body: NewAccountsAssociationRequestSchema,
+    ):
+        if not app_id:
+            raise ValueError('Missing app_id')
+        return self.http_post(url=f'/v2/apps/{app_id}/association/accounts', body=body.json())
+
+    def create_groupings_association(
+        self,
+        app_id: str,
+        body: NewGroupsAssociationRequestSchema,
+    ):
+        if not app_id:
+            raise ValueError('Missing app_id')
+        return self.http_post(url=f'/v2/apps/{app_id}/association/groupings', body=body.json())
+
+    def create_assets(
+        self,
+        app_id: str,
+        body: NewAssetsRequestSchema,
+    ):
+        if not app_id:
+            raise ValueError('Missing app_id')
+        return self.http_post(url=f'/v2/apps/{app_id}/assets', body=body.json())
+
+    def create_assets_inheritance(
+        self,
+        app_id: str,
+        body: NewAssetsInheritanceRequestSchema,
+    ):
+        if not app_id:
+            raise ValueError('Missing app_id')
+        return self.http_post(url=f'/v2/apps/{app_id}/assets/inheritance', body=body.json())
