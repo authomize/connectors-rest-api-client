9f71886e53938d3a5c614a18219cba0047ba583f
RoiAuthomize <90147027+RoiAuthomize@users.noreply.github.com>
2022-08-24 17:44:56 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 if __name__ == '__main__':
     setup(
-        version='2.1.0',
+        version='2.2.0',
         name='authomize-rest-api-client',
         author='Authomize inc.',
         license='MIT',
