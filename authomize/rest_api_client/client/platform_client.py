6a0644b9ccf587131611b59082ddaaed950f8ca9
RoiAuthomize <90147027+RoiAuthomize@users.noreply.github.com>
2023-02-13 21:39:00 +0200

    Create retrieve incident endpoint (#29)

--- authomize/rest_api_client/client/platform_client.py
+++ authomize/rest_api_client/client/platform_client.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 from authomize.rest_api_client.client.base_client import BaseClient
 from authomize.rest_api_client.generated.external_rest_api.schemas import (
+    IncidentExpansion,
     IsAliveResponse,
     MeResponse,
+    NonPaginatedResponseSchemaIncidentSchema,
 )
 
 
@@ -18,3 +22,20 @@ class PlatformClient(BaseClient):
 
     def me(self) -> MeResponse:
         return self.http_get('/me')
+
+    def retrieve_incident(
+        self,
+        app_id: str,
+        incident_id: str,
+        expand: Optional[list[IncidentExpansion]] = None,
+    ) -> NonPaginatedResponseSchemaIncidentSchema:
+        if not app_id:
+            raise ValueError('Missing app_id')
+        if not incident_id:
+            raise ValueError('Missing incident_id')
+        params = None
+        if expand:
+            params = dict(
+                expand=expand,
+            )
+        return self.http_get(f'/v2/incidents/{incident_id}', params=params)
