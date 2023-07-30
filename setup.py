b46fee309889fb4bb3641912c4bde8e78a9bcd17
Jenkins CI <jenkins@authomize.com>
2023-07-30 08:58:27 +0000

    Bumped version to 4.3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 if __name__ == '__main__':
     setup(
-        version='4.3.6',
+        version='4.3.7',
         name='authomize-rest-api-client',
         author='Authomize inc.',
         license='MIT',
